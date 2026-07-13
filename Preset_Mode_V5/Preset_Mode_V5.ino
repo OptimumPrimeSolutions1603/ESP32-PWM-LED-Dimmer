653a0ceb5d59a6d0cbe0a4d7e7289d8550776bca
Aditya Srivastava <aditya160307@gmail.com>
2026-07-13 20:18:58 +0530

    Preset Button added

---
+++ Preset_Mode_V5/Preset_Mode_V5.ino
@@ -0,0 +1,156 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Screen dimensions
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+//COnfiguring the buttons
+#define LED_PIN 4
+const int inc_button=18;
+const int dec_button=5;
+const int mode_button=19;
+
+//PWM
+const int frequency=5000;
+const int resolution=8;
+
+int brightnessPercent=0;
+int brightness=0;
+
+int mode=0;
+
+//States
+bool incCurrent=HIGH;
+bool incPrevious=HIGH;
+
+bool decCurrent=HIGH;
+bool decPrevious=HIGH;
+
+bool modeCurrent=HIGH;
+bool modePrevious=HIGH;
+
+void updateDisplay()
+{
+  display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setCursor(0, 0);
+  display.setTextColor(SSD1306_WHITE);
+
+  int percent=brightness*100/255;
+  
+  display.println("Brightness");
+
+  display.setTextSize(3);
+  display.setCursor(35, 30);
+  display.print(brightnessPercent);
+  display.print("%");
+
+  display.drawRect(10, 54, 108, 8, SSD1306_WHITE);
+  int barWidth=brightnessPercent*108/100;
+  display.fillRect(10, 54, barWidth, 8, SSD1306_WHITE);
+
+  display.display();
+
+}
+
+void setup() {
+  Serial.begin(115200);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println("OLED failed");
+    while(true);
+  }
+  pinMode(inc_button, INPUT_PULLUP);
+  pinMode(dec_button, INPUT_PULLUP);
+  pinMode(mode_button, INPUT_PULLUP);
+
+  ledcAttach(LED_PIN, frequency, resolution);
+  ledcWrite(LED_PIN, brightness);
+
+  updateDisplay();
+
+}
+
+void loop() {
+  incCurrent=digitalRead(inc_button);
+  decCurrent=digitalRead(dec_button);
+  modeCurrent=digitalRead(mode_button);
+
+  if(incPrevious==HIGH && incCurrent==LOW)
+  {
+    delay(30);
+
+    if (digitalRead(inc_button) == LOW)
+    {
+      brightnessPercent+=10;
+      if(brightnessPercent>100)
+        brightnessPercent=100;
+    
+      brightness=brightnessPercent*255/100;
+      ledcWrite(LED_PIN, brightness);
+      updateDisplay();
+    }
+  }
+
+  if(decPrevious==HIGH && decCurrent==LOW)
+  {
+    delay(30);
+    if(digitalRead(dec_button) == LOW)
+    {
+      brightnessPercent-=10;
+      if(brightnessPercent<0)
+        brightnessPercent=0;
+    
+      brightness=brightnessPercent*255/100;
+      ledcWrite(LED_PIN, brightness);
+      updateDisplay();
+      delay(30);
+    }
+  }
+
+  if(modePrevious==HIGH && modeCurrent==LOW)
+  {
+    delay(30);
+    if(digitalRead(mode_button)==LOW)
+    {
+      mode++;
+        if(mode>4)
+          mode=0;
+      switch(mode)
+      {
+        case 0:
+          brightnessPercent=0;
+          break;
+
+        case 1:
+          brightnessPercent=25;
+          break;
+          
+        case 2:
+          brightnessPercent=50;
+          break;
+          
+        case 3:
+          brightnessPercent=75;
+          break;
+          
+        case 4:
+          brightnessPercent=100;
+          break;
+      }
+    }
+    brightness=brightnessPercent*255/100;
+    ledcWrite(LED_PIN, brightness);
+    updateDisplay();
+  }
+
+  incPrevious=incCurrent;
+  decPrevious=decCurrent;
+  modePrevious=modeCurrent;
+}
+
